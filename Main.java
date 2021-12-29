30b85e960f7413c791aa09d7cc3be51edc215c32
Bluesfellow <bluesfellow107@gmail.com>
2021-12-29 19:18:02 +0900

    백준 10718번.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 package baekjoon;
 
-public class HelloWorld {
+public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
+		System.out.println("강한친구 대한육군");
 
 	}
 
