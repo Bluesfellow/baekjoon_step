9f57147fbbecfeb76b4116bbd20c09554be40f64
IHK <86643209+Bluesfellow@users.noreply.github.com>
2021-12-30 20:07:51 +0900

    Create 백준 10171.java

---
+++ 1. 입출력과 사칙연산/백준 10171.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+      //백슬래시 한번을 쓰게 되면 Escape Sequence가 발생하므로 두번 써서 이를 방지할 수 있음
+    }
+}
