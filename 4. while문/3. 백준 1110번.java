11f78137d34e39fade8464568fb329f19aac1a50
IHK <86643209+Bluesfellow@users.noreply.github.com>
2022-01-14 15:36:41 +0900

    Update 3. 백준 1110번.java

--- 4. while문/3. 백준 1110번.java
+++ 4. while문/3. 백준 1110번.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		int cnt = 0; // 사이클 카운트 변수
 		int N_Copy = N; // 초기 N값을 담을 변수
@@ -18,9 +19,10 @@ public class Main {
 				break;
 			}
 		}
-
-		System.out.print(cnt);
-
+		bw.write(String.valueOf(cnt));	// 또는 bw.write(cnt + "");
+		
 		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
