5b156fa8f36fcd62cb271f061a0b21ea01fe644c
IHK <86643209+Bluesfellow@users.noreply.github.com>
2022-01-19 22:57:23 +0900

    Create 1. 백준 11654번.java

---
+++ 7. 문자열/1. 백준 11654번.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		char ch = scanner.next().charAt(0);
+
+		int num = (int) ch;
+
+		System.out.print(num);
+	}
+}
