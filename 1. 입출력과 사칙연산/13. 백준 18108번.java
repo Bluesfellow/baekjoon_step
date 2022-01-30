d00d5aadd26b81ef81ef04319582e1abe68022bc
IHK <86643209+Bluesfellow@users.noreply.github.com>
2022-01-30 23:43:56 +0900

    Create 13. 백준 18108번.java

---
+++ 1. 입출력과 사칙연산/13. 백준 18108번.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int y = scanner.nextInt();
+
+		System.out.print(y - 543);
+	}
+
+}
